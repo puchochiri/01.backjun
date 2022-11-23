ea38b8ddcb8199358e8caadec341cec5fbc501da
puchochiri <doawishfor@naver.com>
2022-11-23 22:04:03 +0900

    commit

---
+++ 99.codingExample/src/No2480/Main.java
@@ -0,0 +1,46 @@
+package No2480;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		//ab가 같을 때
+		if(a==b) {
+			
+			//bc가 같을 때
+			if(b==c) {
+				
+				//ac가 같을 때 
+				if(a==c) {
+					
+				//ac가 다를 때	
+				} else if(a!=c) {
+					
+				}
+				
+				//bc가 다를때
+			} else if(b!=c) {
+				
+				//ac가 같을때
+				if(a==c) {
+					
+				} else if(a!=c) {
+					
+				}
+				
+			}
+		}
+		
+		
+		
+	
+	
+		
+	}	
+}
