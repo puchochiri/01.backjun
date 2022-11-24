af539608ccb65960ff2ef9c013c0b1b879e31c1a
H2210756 <H2210756@HMC-YH-N041096.hkmc.hkmg.global>
2022-11-24 16:38:57 +0900

    commit

--- 99.codingExample/src/No2480/Main.java
+++ 99.codingExample/src/No2480/Main.java
@@ -9,35 +9,65 @@ public class Main {
 		a = sc.nextInt();
 		b = sc.nextInt();
 		c = sc.nextInt();
+		int x = 0;
+		int max = 0;
+
 		
-		//ab가 같을 때
+		
+		
+		// ab가 같을 때
 		if(a==b) {
 			
-			//bc가 같을 때
+			//bc가 같을 때 ==> 3개가 동일
 			if(b==c) {
 				
-				//ac가 같을 때 
-				if(a==c) {
-					
-				//ac가 다를 때	
-				} else if(a!=c) {
-					
-				}
+				x = 10000 + a * 1000;
 				
-				//bc가 다를때
+		
+				
+				//bc가 다를때 ==> 2개가 동일
 			} else if(b!=c) {
 				
-				//ac가 같을때
+				x = 1000 + a * 100;
+				
+			
+				
+			}
+		// ab가 다를 때
+		} else if (a!=b) {
+			
+			//bc가 같음 ==> 2개가 동일
+			if(b==c) {
+				
+				x = 1000 + b * 100;
+				
+			// bc가 다름
+			} else if(b!=c) {
+				
+				
+				// ac가 같을때 ==> 2개가 동일
 				if(a==c) {
-					
+					x = 1000 + a * 100;
+
+					// ac가 다를때 ==> 모두 다름
 				} else if(a!=c) {
-					
+					int [] arr = { a ,b ,c };
+					for (int i = 0; i < arr.length; i++) {
+						if(arr[i] > max) {
+							max = arr[i];
+						}
+						x = max * 100; 
+						
+					}
+
 				}
 				
 			}
+			
+			
 		}
 		
-		
+		System.out.println(x);
 		
 	
 	
